1283252631baeb5ce6eb4fae71b6a22e57dbb9c5
komalasrani <87267706+komalasrani@users.noreply.github.com>
2022-11-03 18:59:01 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -34,9 +34,14 @@ class App extends Component<{}, IState> {
   /**
    * Render Graph react component with state.data parse as property data
    */
-  renderGraph() {
+/**
+ * Render Graph react component with state.data parse as property data
+ */
+renderGraph() {
+  if (this.state.showGraph){
     return (<Graph data={this.state.data}/>)
   }
+}
 
   /**
    * Get new data from server and update the state with the new data
